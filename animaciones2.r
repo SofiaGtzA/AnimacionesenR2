568ad0ed1e67b66428bb5b6ed02bd5757ee8ba4d
SofiaGtzA <A01177186@itesm.mx>
2021-05-19 13:34:00 -0500

    Create animaciones2.r
    Se genero un codigo de R para leer la base de datos de gapminder que contiene informacion del PIB de diferentes bases y su esperanza de vida

---
+++ animaciones2.r
@@ -0,0 +1,12 @@
+rm(list = ls())
+library(ggplot2)
+library(gapminder)
+
+#asignar a la variable datos la tabla de gapminder
+datos <- gapminder
+
+#crear grafico
+grafico <- ggplot(datos, aes(x = gdpPercap, y=lifeExp, colour = continent)) + geom_point()
+
+#mostrar grafico
+grafico
